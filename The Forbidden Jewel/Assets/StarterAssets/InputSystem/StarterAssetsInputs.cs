90861bae941bccc55cdc661ae84e90328f3e0752
Cameron Washington <89416312+C00439696@users.noreply.github.com>
2023-03-15 00:56:00 -0500

    This better

--- The Forbidden Jewel/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ The Forbidden Jewel/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -112,10 +112,7 @@ namespace StarterAssets
 			SetCursorState(cursorLocked);
 		}
 
-		private void SetCursorState(bool newState)
-		{
-			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-		}
-	}
+        private void SetCursorState(bool newState) => Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+    }
 	
 }
